598f6c7a2826a4f78f85e51ab55e6446dd7a66ee
Jacob <43556549+Jachan-12@users.noreply.github.com>
2021-04-29 17:54:31 -0700

    Completed settings, goal & buttons

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import "./styles.css";
+import React, { useState } from "react";
 
 export default function App() {
+  const [goal, setGoal] = useState(
+    "Ren's parents have been kidnapped by goblins, go on an expedition to save them"
+  );
+  const [setting, setSetting] = useState(
+    "You start out at a small village, near an ocean and a huge forest."
+  );
+  const [option1, setOption1] = useState("Set out straight away");
+  const [option2, setOption2] = useState("Get prepared first");
+
+  function clickOption1() {
+    console.log("hi");
+  }
+  function clickOption2() {
+    console.log("clicked");
+  }
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>Ren's Adventure</h1>
+      <h2>{goal}</h2>
+      <h3>{setting}</h3>
+      <button onClick={clickOption1}>{option1}</button>
+      <button onClick={clickOption2}>{option2}</button>
     </div>
   );
 }
